0e017b5eb05bf84a4fb5c7732a658a5c51d60905
pratiksj <pratiksharai728@gmail.com>
2023-11-02 14:10:35 +0545

    feat:create footer component and css file

---
+++ frontend/src/Components/Footer/Footer.jsx
@@ -0,0 +1,35 @@
+import './Footer.css'
+
+export const Footer = () => {
+  return (
+    <div className='footer'>
+
+        <div className="footer-logo">
+            <img src='' alt=''/>
+            <p>Shopper</p>
+        </div>
+        <ul className="footer-links">
+        <li>Company</li>
+        <li>Product</li>
+        <li>Offices</li>
+        <li>About</li>
+            <li>Contact</li>
+        </ul>
+        <div className="footer-socials-icon">
+            <div className="footer-icons-container">
+                <i className='fa fa-facebook'></i>
+            </div>
+            <div className="footer-icons-container">
+                <i className='fa fa-instagram'></i>
+            </div>
+            <div className="footer-icons-container">
+                <i className='fa fa-whatsapp'></i>
+            </div>
+        </div>
+        <div className="footer-copyright">
+            <hr/>
+            <p>Copyright @ 2023-All right Reserved</p>
+        </div>
+    </div>
+  )
+}
--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -1,3 +1,4 @@
+
 import { Hero } from "../Components/Hero/Hero"
 import { NewCollections } from "../Components/NewCollections/NewCollections"
 import { NewsLetter } from "../Components/NewsLetter/NewsLetter"
@@ -14,6 +15,7 @@ export const Shop = () => {
       <NewCollections/>
 
       <NewsLetter/>
+      
     </div>
   )
 }
