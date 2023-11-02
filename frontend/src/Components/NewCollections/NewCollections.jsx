2ff3e3dd30e6fd2419a65acdb86f597eb5579d6f
pratiksj <pratiksharai728@gmail.com>
2023-11-02 11:19:29 +0545

    feat:add images

---
+++ frontend/src/Components/NewCollections/NewCollections.jsx
@@ -0,0 +1,12 @@
+import './NewCollections.css'
+
+export const NewCollections = () => {
+  return (
+    <div className="new-collections" >
+<h1>New Collections</h1>
+<hr/>
+<div className="collections"></div>
+
+    </div>
+  )
+}
